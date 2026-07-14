8211008dc46afd607c1a5abd819c440ac9d64dda
Aazer <Ownersmith17@gmail.com>
2026-07-14 13:22:40 +0000

    feat: configure expo-router root layout and global providers

---
+++ mobile/src/app/(auth)/_layout.jsx
@@ -0,0 +1,8 @@
+import { View, Text, Stack } from 'expo-router'
+import React from 'react'
+
+export default function AuthLayout() {
+  return (
+    <Stack screenOptions={{headerShown: false}}/>
+  )
+}
--- mobile/src/app/index.jsx
+++ mobile/src/app/index.jsx
@@ -1,12 +1,15 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Image } from "expo-image";
+import { Link } from "expo-router";
 
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Hello</Text>
-      <Image source={require("")} />
+
+      <Link href="/(auth)/signup">Signup</Link>
+      <Link href="/(auth)">Login</Link>
     </View>
   );
 }
