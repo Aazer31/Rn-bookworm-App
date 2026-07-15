247e9d14f74c919aa0b432654ab35fd7f494a5f5
Aazer <Ownersmith17@gmail.com>
2026-07-15 13:35:46 +0000

    feat: implement automated cron jobs for server maintenance

--- mobile/store/authStore.js
+++ mobile/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-// Ton API de production unique sur Render !
+
 const API_URL = "https://bookworm-api-r5xu.onrender.com";
 
 export const useAuthStore = create((set) => ({
